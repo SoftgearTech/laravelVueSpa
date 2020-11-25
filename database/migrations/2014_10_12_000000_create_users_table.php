6bb44f580817dbbf3472a02e348e1b1a28f3cdc0
antcoder15402 <66098676+antcoder15402@users.noreply.github.com>
2020-11-25 14:48:19 +0500

    Reset Password Added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('user');
+            $table->string('verification_code')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
