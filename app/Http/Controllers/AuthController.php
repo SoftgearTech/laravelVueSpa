ec1f716d37cba9b9b523f1f42affe822f0d14900
antcoder15402 <66098676+antcoder15402@users.noreply.github.com>
2020-11-25 14:37:40 +0500

    Update AuthController.php
    Reset Password Added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 use App\User;
 use Auth;
 use Carbon\Carbon;
+use Mail;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email',
+            'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed'
         ]);
 
@@ -96,5 +97,90 @@ class AuthController extends Controller
             'message' => 'Not loggedin',
             'status_code' => 500
         ], 500);
+    }
+
+    public function resetPasswordRequest(Request $request) {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'We have sent a verification code to your email address',
+                'status_code' => 200
+            ], 200);
+        } else {
+            $random = rand(111111, 999999);
+
+            $user->verification_code = $random;
+            if ($user->save()) {
+                $userData = array(
+                    'email' => $user->email,
+                    'full_name' => $user->name,
+                    'random' => $random
+                );
+
+                Mail::send('emails.reset_password', $userData, function ($message) use ($userData) {
+                    $message->from('no-reply@laravel.vue.learning', 'Password Request');
+                    // $message->sender('john@johndoe.com', 'John Doe');
+                    $message->to($userData['email'], $userData['full_name']);
+                    // $message->cc('john@johndoe.com', 'John Doe');
+                    // $message->bcc('john@johndoe.com', 'John Doe');
+                    // $message->replyTo('john@johndoe.com', 'John Doe');
+                    $message->subject('Reset Password Request (Laravel Vue)');
+                    // $message->priority(3);
+                    // $message->attach('pathToFile');
+                });
+
+                if (Mail::failures()) {
+                    return response()->json([
+                        'message' => 'Some error occurred, Please try again',
+                        'status_code' => 500
+                    ], 500);
+                } else {
+                    return response()->json([
+                        'message' => 'We have sent a verification code to your email address',
+                        'status_code' => 200
+                    ], 200);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'Some error occurred, Please try again',
+                    'status_code' => 500
+                ], 500);
+            }
+        }
+    }
+
+    public function resetPassword(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'verification_code' => 'required|integer',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user = User::where('email', $request->email)->where('verification_code', $request->verification_code)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found/Invalid code',
+                'status_code' => 401
+            ], 401);
+        } else {
+            $user->password = bcrypt(trim($request->password));
+            $user->verification_code = Null;
+
+            if ($user->save()) {
+                return response()->json([
+                    'message' => 'Password updated successfully!',
+                    'status_code' => 200
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Some error occurred, Please try again',
+                    'status_code' => 500
+                ], 500);
+            }
+        }
     }
 }
